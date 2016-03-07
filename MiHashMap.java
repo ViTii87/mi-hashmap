6a7f73b7e21838d1baf33202021276452b056d5e
Victor <victormel87@gmail.com>
2016-03-07 12:27:21 +0100

    Añadido constructor y un par de atributos

--- MiHashMap.java
+++ MiHashMap.java
@@ -7,27 +7,18 @@
  */
 public class MiHashMap
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // Dos arrays para simular el hashmap
+    private String[] claves;
+    private int[] valores;
 
     /**
      * Constructor for objects of class MiHashMap
      */
     public MiHashMap()
     {
-        // initialise instance variables
-        x = 0;
+        this.claves = new String[0];
+        this.valores = new int[0]; 
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+ 
 }
